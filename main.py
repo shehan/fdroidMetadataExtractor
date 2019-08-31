4f06813beb25be3cbecfe92e907fc0e17db8fe0b
shehan <shehan.peruma@gmail.com>
2019-08-30 21:23:31 -0400

    updated

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import os
 import glob
 import sys
+import stat
+import argparse
+import shutil
 import git
 from git import Repo
 import yaml
@@ -8,40 +11,74 @@ import sqlite3
 
 
 fdroid_repo_remote = "https://gitlab.com/fdroid/fdroiddata.git"
-fdroid_repo_local = 'tmp/fdroid'
+fdroid_repo_local = 'tmp_fdroid'
 fdroid_repo_local_metadata = fdroid_repo_local+'/metadata'
 results_output = 'output'
 results_output_db_name = 'results.sqlite'
 results_output_db_path = results_output+'/' + results_output_db_name
 
+parser = argparse.ArgumentParser(description='F-Droid Metadata Extractor')
+parser.add_argument('-o', action="store_true", default=False,
+                    dest='arg_overwrite', help='Overwrite all data')
+args = parser.parse_args()
+
+
+def remove_readonly(func, path, excinfo):
+    os.chmod(path, stat.S_IWRITE)
+    func(path)
+
+if args.arg_overwrite:
+    if os.path.exists(fdroid_repo_local):
+        print('removing fdroid directory')
+        shutil.rmtree(fdroid_repo_local, onerror=remove_readonly)
+    if os.path.exists(results_output):
+        print('removing output directory')
+        shutil.rmtree(results_output,  onerror=remove_readonly)
+
+
 if not os.path.exists(fdroid_repo_local):
+    print('creating fdroid directory')
     os.makedirs(fdroid_repo_local)
 
 if not os.path.exists(results_output):
+    print('creating output directory')
     os.makedirs(results_output)
 
 if not os.path.exists(results_output_db_path):
+    print('creating empty database')
     conn = sqlite3.connect(results_output_db_path)
     c = conn.cursor()
     c.execute('''CREATE TABLE App
-        ([Id] INTEGER PRIMARY KEY,[Name] text,[AutoName] text,[WebSite] text,[AuthorName] text,[AuthorEmail] text,[Categories] text,[Repo] text,[Description] text)''')
+        ([Id] INTEGER PRIMARY KEY AUTOINCREMENT,[UniqueName] text,[Name] text,[AutoName] text,[WebSite] text,[AuthorName] text,[AuthorEmail] text,[Categories] text,[Repo] text,[SourceCode] text,[RepoType] text,[Changelog] text,[License] text,[IssueTracker] text,[Description] text)''')
     conn.commit()
-
+    conn.close()
 
 try:
     repo = git.Repo(fdroid_repo_local, search_parent_directories=False)
 except git.exc.InvalidGitRepositoryError:
-    print('fdroid repo not cloned')
-    print('cloning fdroid repo...')
+    print('cloning fdroid repo')
     repo = Repo.clone_from(fdroid_repo_remote, fdroid_repo_local)
-    print('fdroid repo cloned')
 
-
+print('extracting metadata')
+conn = sqlite3.connect(results_output_db_path)
+c = conn.cursor()
 os.chdir(fdroid_repo_local_metadata)
 for file in glob.glob("*.yml"):
     with open(file, 'rt', encoding='utf8') as stream:
         try:
             data = yaml.safe_load(stream)
+            data_repotype = data_changelog = data_license = data_issuetracker = data_sourcecode = data_repo = data_autoname = data_name = data_description = data_categories = data_website = data_authorname = data_authoremail = ''
+            data_uniquename = file.rsplit(".", 1)[0]
+            if 'RepoType' in data:
+                data_repotype = data['RepoType']
+            if 'Changelog' in data:
+                data_changelog = data['Changelog']
+            if 'License' in data:
+                data_license = data['License']
+            if 'IssueTracker' in data:
+                data_issuetracker = data['IssueTracker']
+            if 'SourceCode' in data:
+                data_sourcecode = data['SourceCode']
             if 'Repo' in data:
                 data_repo = data['Repo']
             if 'AutoName' in data:
@@ -58,8 +95,16 @@ for file in glob.glob("*.yml"):
                 data_authorname = data['AuthorName']
             if 'AuthorEmail' in data:
                 data_authoremail = data['AuthorEmail']
+
+            c.execute('INSERT INTO App (Repo,AutoName,Name,Description,WebSite,AuthorName,AuthorEmail,SourceCode,IssueTracker,License,Changelog,RepoType,UniqueName,Categories) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?);',
+                      (data_repo, data_autoname, data_name, data_description, data_website, data_authorname, data_authoremail, data_sourcecode, data_issuetracker, data_license, data_changelog, data_repotype, data_uniquename, ','.join(data_categories)))
+            conn.commit()
+
         except yaml.YAMLError as exc:
             print(exc)
         except:
             e = sys.exc_info()[0]
             print(e)
+conn.close()
+
+print('execution completed!')
